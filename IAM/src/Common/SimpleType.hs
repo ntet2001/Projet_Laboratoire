d82f0aedfd1494d239403f15ea0329cad21424e1
raoul1609 <elieraoulnet@gmail.com>
2023-01-28 14:08:08 +0100

    updated types in simpleType.hs

--- IAM/src/Common/SimpleType.hs
+++ IAM/src/Common/SimpleType.hs
@@ -1,18 +1,4 @@
-module Common.SimpleType 
-(
-Nom, 
-NomOp, 
-PrenomOp, 
-Matricule,
-Identifiant,
-Email(..),
-PasswordOp,
-Photo,
-Operateur(..),
-Visibilite(..),
-Statut(..)   
-)
-where
+module Common.SimpleType where
 
     {---------------------------=== Importation ===----------------------------------}
     import qualified Data.ByteString.Char8 as C    
@@ -21,11 +7,18 @@ where
     type Nom = String 
     type NomOp = Nom 
     type PrenomOp = Nom 
+    type NomPatient = Nom
+    type PrenomPatient = Nom
     type Matricule = String
     type Identifiant = String
     data Statut = Connecter | Deconnecter deriving (Show,Read)
     data Visibilite = Oui | Non deriving (Show,Read,Eq)
-    data Email = MkEmail { identifiant :: Identifiant, domaine :: String, extension :: String } deriving (Show ,Read)
+    data Email = MkEmail { identifiant :: Identifiant, domaine :: String, extension :: String } deriving (Show ,Read, Eq)
     type PasswordOp = String
     type Photo = C.ByteString
-    data Operateur = MKOperateur { nomOp :: NomOp, prenomOp :: PrenomOp, matricule::Matricule, email :: Email, passwordOp :: PasswordOp, photo :: Photo, visibilite :: Visibilite, statut :: Statut } deriving (Show,Read) 
+    data Operateur = MKOperateur { nomOp :: NomOp, prenomOp :: PrenomOp, matricule::Matricule, 
+        email :: Email, passwordOp :: PasswordOp, photo :: Photo, visibilite :: Visibilite, statut :: Statut } deriving (Show,Read) 
+    
+    data Patient = MkPatient {nameOf :: Nom , postNameOf :: Nom , 
+                emailOf :: Email, photoOf :: Photo, code :: Int } deriving (Show , Read , Eq)
+    
