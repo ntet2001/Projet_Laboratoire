5b219882accd796f79d1c9c9957de77819022079
raoul1609 <elieraoulnet@gmail.com>
2023-01-30 09:10:44 +0100

    crud patient

---
+++ IAM/src/App/AppPatient.hs
@@ -0,0 +1,26 @@
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+module App.AppPatient where 
+
+import System.Random
+import Text.ParserCombinators.Parsec
+
+-- fonction pour generer un code patient qui doit avoir au moins 4 chiffres 
+
+genCode :: IO Int
+genCode = do
+    generator <- newStdGen 
+    let (x,_) = randomR (0000,9999) generator
+    return x
+ 
+verifCode :: Int -> Either ParseError Int  
+verifCode codeAcces = do
+    let x = show codeAcces
+    result <- parse parserCode "" x
+    return result 
+
+parserCode :: Parser Int 
+parserCode = do 
+    x <- many1 digit 
+    case (length x) == 4 of 
+        True -> return (read x :: Int) 
+        False -> unexpected "code invalide " 
---
+++ IAM/src/Domain/DomPatient.hs
@@ -0,0 +1,46 @@
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+module Domaine.DomPatient where 
+
+import App.AppPatient 
+import Common.SimpleType
+import App.AppFile (verifEmail, verifNomEtPrenom, verifEmail, verifPhoto) 
+import Text.ParserCombinators.Parsec
+
+
+
+-- fonction pour creer un patient, le refactoring se fera avec le monad tranformer 
+createPatient :: NomPatient -> PrenomPatient -> String -> String  -> IO Patient 
+createPatient x y k j = do 
+    code <- genCode
+    let var = createHelper x y k j code 
+    case var of 
+        Right someStuff -> return someStuff
+        Left erreur -> error $ show erreur 
+
+-- fonction d'aide a la fonction pour creer un patient 
+createHelper :: NomPatient -> PrenomPatient -> String -> String -> Int -> Either ParseError Patient 
+createHelper nom prenom email tof val = do  
+    MkPatient <$> verifNomEtPrenom nom
+              <*> verifNomEtPrenom prenom
+              <*> verifEmail email
+              <*> verifPhoto tof 
+              <*> verifCode val
+
+-- verifier si un patient existe deja avant de l'enregistrer 
+
+verifPatient :: Patient -> String -> IO (Either String Patient) 
+verifPatient inputPatient nameFile = do
+    contient <- readFile nameFile 
+    let ligneParligne = lines contient
+        convertToPatient = fmap read ligneParligne :: [Patient]
+        --listInfosPatient = map infos convertToPatient :: [InfoPatient]
+        var = emailOf inputPatient 
+        emailOfPatient = fmap emailOf convertToPatient
+        listeEmail = filter (== var)  emailOfPatient
+    if null listeEmail then return $ Right inputPatient
+      else return $ Left "ce patient existe deja !!"
+
+
+
+
+        
