3260681b7d823f9dc52d641708faadd22f5a97c6
raoul1609 <elieraoulnet@gmail.com>
2023-02-23 11:15:35 +0100

    modified SimpleTypes.hs

--- EFA/src/Common/SimpleTypes.hs
+++ EFA/src/Common/SimpleTypes.hs
@@ -12,27 +12,32 @@ module Common.SimpleTypes where
     import Servant
     import qualified Data.Text as T 
     import Data.Time
+    import Data.Aeson 
+    import GHC.Generics
 
     {-======================== Types Definitions =============================-}
 
-    data Categorie = Biochimie | Hematologie | Serologie | Parasitologie
+    data Categorie = Biochimie | Hematologie | Serologie | Parasitologie deriving (Show, Eq, Read)
 
-    data Analyse = MkAnalyse {idAnalyse :: String,
+    data Analyse a = MkAnalyse {idAnalyse :: String,
         nomAnalyse :: String,
-        valUsuel :: ValUsuel,
+        valUsuel :: ValUsuel a,
         categorie :: Categorie
-    }
+    } deriving (Show, Eq, Read)
 
 
-    data ValUsuel = Vide | UneVal | Interval Float Float
+    data ValUsuel a  = Vide | UneVal a  | Interval Float Float deriving (Show, Eq, Read)
 
 
     data Fiche = MkFIche { idFiche::String,
         infoPatient :: InfoPatient,
         analyses :: [String],
         prescripteur :: String,
-        date :: UTCTime
-    }
+        date :: UTCTime 
+    } deriving (Show, Eq, Read, Generic)
+
+    instance ToJSON Fiche 
+    instance FromJSON Fiche 
  
 
     data InfoPatient = MkPatient { nom :: String,
@@ -40,5 +45,11 @@ module Common.SimpleTypes where
         datenaissance :: UTCTime,
         genre :: String, 
         email :: String
-    }
+    } deriving (Show, Eq, Read, Generic)
+
+    instance ToJSON InfoPatient
+    instance FromJSON InfoPatient
+
+
+
 
